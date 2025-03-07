15c9e98157f6e3a82c7f0ac287faf79702ff4709
Corentin Gautier <corentin@easy-node.xyz>
2025-03-07 10:23:48 +0100

    use terser

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import terser from '@rollup/plugin-terser';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
@@ -16,19 +17,38 @@ export default defineConfig(({ command, mode }) => {
       },
       outDir: '../dist',
       rollupOptions: {
-        external: ['window', 'document'],
-        output: {
-          globals: {
-            window: 'window',
-            document: 'document',
-            IntersectionObserver: 'IntersectionObserver',
-            MutationObserver: 'MutationObserver',
-            CustomEvent: 'CustomEvent',
-            requestIdleCallback: 'requestIdleCallback'
+        external: ['window', 'document', 'IntersectionObserver', 'MutationObserver', 'CustomEvent', 'requestIdleCallback'],
+        output: [
+          {
+            format: 'es',
+            entryFileNames: 'snap-carousel.js'
+          },
+          {
+            format: 'iife',
+            name: 'SnapCarousel',
+            entryFileNames: 'snap-carousel.min.js',
+            plugins: [
+              terser({
+                format: {
+                  comments: false
+                },
+                compress: {
+                  drop_console: true,
+                  drop_debugger: true
+                }
+              })
+            ]
           }
+        ],
+        globals: {
+          window: 'window',
+          document: 'document',
+          IntersectionObserver: 'IntersectionObserver',
+          MutationObserver: 'MutationObserver',
+          CustomEvent: 'CustomEvent',
+          requestIdleCallback: 'requestIdleCallback'
         }
       },
-      minify: 'esbuild',
       sourcemap: false
     } : {
       outDir: '../docs',
