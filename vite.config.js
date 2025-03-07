7030b60c730995c410a3345628bc4c687e2fe907
Corentin Gautier <corentin@easy-node.xyz>
2025-03-07 02:53:22 +0100

    fix docs assets

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig(({ command, mode }) => {
 
   return {
     root: 'src',
+    base: isLibrary ? '/' : '',
     build: isLibrary ? {
       lib: {
         entry: resolve(__dirname, 'src/snap-carousel.js'),
